b4f7cd872943d38e08a3c3714a137bb1bf4c019f
ishanvas <ishanvas@gmail.com>
2014-10-30 06:18:47 +0000

    bug fixes

--- gabriel-master/gabriel/proxy/common.py
+++ gabriel-master/gabriel/proxy/common.py
@@ -326,7 +326,7 @@ class ResultpublishClient(threading.Thread):
                 sock.settimeout(1)
                 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-		addr ='10.2.12.3';
+		addr ='10.2.12.4';
                 sock.connect((addr, port))
                 sock.settimeout(original_timeout)
                 # update
